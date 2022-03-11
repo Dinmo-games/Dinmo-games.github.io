53a87b1f8ef61497a367792b6065dbe1ab14c881
Dinmo-games <dfnstr@yandex.com>
2022-03-11 17:36:46 +0500

    Check Unity_Logo

--- Games/Puzzles/Color_Blocks_1010/TemplateData/start.js
+++ Games/Puzzles/Color_Blocks_1010/TemplateData/start.js
@@ -1,8 +1,6 @@
 var $start_container = document.querySelectorAll(".start_container");
 
 function Hide_Start_Screen() {
-    // document.getElementById("play_game").onclick = Hide_Start_Screen();
-    console.log("HIDE");
     $start_container[0].style.zIndex = -100;
     $start_container[0].style.display = "none";
 }

cd43436a68dd4255dbafd16f2df6468fe8ba04d4
Dinmo-games <dfnstr@yandex.com>
2022-03-11 13:52:34 +0500

    upd_1

--- Games/Puzzles/Color_Blocks_1010/TemplateData/start.js
+++ Games/Puzzles/Color_Blocks_1010/TemplateData/start.js
@@ -1,4 +1,6 @@
 var $start_container = document.querySelector(".start_container");
+document.getElementById("open_group").onclick = OpenGroup();
+document.getElementById("play_game").onclick = Hide_Start_Screen();
 
 function Hide_Start_Screen() {
     $start_container[0].style.zIndex = -100;
@@ -8,4 +10,10 @@ function Hide_Start_Screen() {
 function Show_Start_Screen() {
     $start_container[0].style.zIndex = 100;
     $mobile_containers[0].style.display = "flex";
+}
+
+
+
+function OpenGroup() {
+    gs.socials.joinCommunity();
 }

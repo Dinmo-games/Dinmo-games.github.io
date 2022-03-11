c0c2c0b5d69018a9e68fe355ca7bd6161845a266
Dinmo-games <dfnstr@yandex.com>
2022-03-11 15:50:27 +0500

    123

--- Games/Puzzles/Color_Blocks_1010/TemplateData/start.js
+++ Games/Puzzles/Color_Blocks_1010/TemplateData/start.js
@@ -1,13 +1,12 @@
-var $start_container = document.querySelector(".start_container");
+var $start_container = document.querySelectorAll(".start_container");
 document.getElementById("open_group").onclick = OpenGroup();
 document.getElementById("play_game").onclick = Hide_Start_Screen();
 
 function Hide_Start_Screen() {
-
-    document.getElementById("play_game").onclick = Hide_Start_Screen();
+    // document.getElementById("play_game").onclick = Hide_Start_Screen();
     console.log("HIDE");
-    // $start_container[0].style.zIndex = -100;
-    // $mobile_containers[0].style.display = "none";
+    $start_container[0].style.zIndex = -100;
+    $mobile_containers[0].style.display = "none";
 }
 
 function Show_Start_Screen() {
